763add89f165419691df68fe4d7bc549b5396881
Mario Gonzalez <mariogonzalez@gmail.com>
2014-01-26 10:48:09 -0500

    Added mobile provision

--- headless-builder.py
+++ headless-builder.py
@@ -7,6 +7,7 @@ import re
 import shutil
 import sys
 import glob
+import zipfile
 
 xcode_project_name = 'UnityCI_xcode';
 cwd = os.path.dirname(os.path.realpath(__file__))  # Note we can't use getcwd() because this file will be called from places other than it's own directory
@@ -41,15 +42,34 @@ def ios_fix_facebook_paths():
 	pbxfile.write(new_pbxstring)
 	pbxfile.close()
 
-print(sys.argv[1])
+def zipdir(path, zip):
+	print(path);
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zip.write(os.path.join(root, file))
 
-#curl http://testflightapp.com/api/builds.json -F file=@UnityCI.ipa -F api_token='58fcc2ee02546d6fe43389734d83d406_MTI0OTA2MDIwMTMtMDgtMjAgMTA6Mzk6MzEuODg1ODcy' -F team_token='2f2b8d10daec740a15818a57edc26e28_MjYxODc4MjAxMy0wOC0yMCAxMDo0MToyNC43NjExNDk'  -F notes='This build was uploaded via the upload API'  -F notify=False 
+def make_ipa:
+	zipf = zipfile.ZipFile(xcode_project_name+'.ipa', 'w')
+    zipdir(cwd+"/_builds/"+xcode_project_name+'/bin/UnityCI.app', zipf)
+    zipf.close()
 
-# # Launch unity builder
-# subprocess.call(['/Applications/Unity/Unity.app/Contents/MacOS/Unity', 
-# 	'-quit',
-# 	'-batchmode',
-# 	'-executeMethod', 'BuilderScript.BuildDevAndProduction', 
-# 	'-projectPath', cwd])
+def upload_to_testflight:
+	subprocess.call(['curl', 
+		'http://testflightapp.com/api/builds.json',
+		'-F', 'file=@UnityCI.ipa',
+		'-F', "api_token='58fcc2ee02546d6fe43389734d83d406_MTI0OTA2MDIwMTMtMDgtMjAgMTA6Mzk6MzEuODg1ODcy'",
+		'-F', "team_token='2f2b8d10daec740a15818a57edc26e28_MjYxODc4MjAxMy0wOC0yMCAxMDo0MToyNC43NjExNDk'",
+		'-F', "notes='This build was uploaded via the upload API'",
+		'-F', "notify=False"])
 
-# ios_fix_facebook_paths()
+# Launch unity builder
+if( sys.argv[1] == "-upload"):
+	subprocess.call(['/Applications/Unity/Unity.app/Contents/MacOS/Unity', 
+		'-quit',
+		'-batchmode',
+		'-executeMethod', 'BuilderScript.BuildDevAndProduction', 
+		'-projectPath', cwd])
+	ios_fix_facebook_paths()
+else:
+	make_ipa()
+	upload_to_testflight()
